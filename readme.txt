e969fd172c6358f160fd99eab36c002fce52ba2c
Studiofreya <Studiofreya@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-12 20:45:05 +0000

    Version 1
    git-svn-id: https://plugins.svn.wordpress.org/sf-category-menu/trunk@1088463 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,47 @@
+=== SF Category Menu ===
+Contributors: Studiofreya AS
+Donate link: http://studiofreya.com/sf-category-menu
+Tags: child, pages, posts, categories, menu, treeview, tree, list
+Requires at least: 3.8
+Tested up to: 3.8.1
+Stable tag: trunk
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Easy treeview menu for WordPress categories.
+
+== Description ==
+Plugin is based on jQuery Treeview and gives an easy Down Menu for WordPress categories. It will display the current page/category and all child pages or categories. 
+[Plugin website](http://www.studiofreya.com/sf-category-menu/)
+
+**Features:**
+* Choose from 5 different styles
+* Exclude categories
+
+**Live demo:**
+[SF Textures](http://sftextures.com)
+
+
+== Installation ==
+Upload the plugin files to the /wp-content/plugins/ directory
+Activate the plugin through the 'Plugins' menu in WordPress
+Use it as a Widget
+
+== Frequently Asked Questions ==
+
+[Plugin page](http://studiofreya.com/sf-category-menu/)
+
+== Screenshots ==
+1. screenshot_admin.png Admin options.
+2. screenshot_plus.png Default style menu created with this plugin.
+3. screenshot_famfamfam.png Famfamfam style menu created with this plugin.
+
+== ChangeLog ==
+
+= 1.0 =
+* Initial release!
+
+== Upgrade Notice ==
+
+= 1.0 =
+No upgrade available.
---
+++ sf-category-menu.php
@@ -0,0 +1,231 @@
+<?php 
+
+/**
+ * Plugin Name: SF Category Menu Widget
+ * Plugin URI: http://studiofreya.com/sf-category-menu/
+ * Description: Easy treeview menu for WordPress categories.
+ * Version: 1.0
+ * Author: Studiofreya AS
+ * Author URI: http://studiofreya.com
+ * License: GPL3
+ */
+
+function getPostCount($catid)
+{
+$args = array(
+	'posts_per_page'   => -1,
+	'offset'           => 0,
+	'category'         => $catid,
+	'orderby'          => 'post_date',
+	'order'            => 'DESC',
+	'include'          => '',
+	'exclude'          => '',
+	'meta_key'         => '',
+	'meta_value'       => '',
+	'post_type'        => 'post',
+	'post_mime_type'   => '',
+	'post_parent'      => '',
+	'post_status'      => 'publish',
+	'suppress_filters' => true );
+
+	$myposts = get_posts( $args );
+
+	$num = count($myposts);
+
+	return $num;
+}
+
+function getSubCategoryPostCount( $catid )
+{
+	$num = 0;
+
+    $args = array(
+        'type'                     => 'post',
+        'parent'                   => "$catid",
+        'orderby'                  => 'name',
+        'order'                    => 'ASC',
+        'hide_empty'               => 1,
+        'hierarchical'             => 0,
+        'exclude'                  => '',
+        'include'                  => '',
+        'number'                   => '',
+        'taxonomy'                 => 'category',
+        'pad_counts'               => true
+    );
+
+    $categories = get_categories( $args );
+
+	foreach($categories as $cat)
+	{
+		$num += $cat->count;
+
+		$num += getSubCategoryPostCount($cat->cat_ID);
+	}
+
+	return $num;
+}
+
+function doCategories( $categories, $select_style, $parent = 0 )
+{
+	$num = count( $categories );
+	
+	if ( $num == 0 )
+	{
+		return;
+	}
+	
+	if ( $parent == 0 )
+	{
+		echo "<ul id='catnavigation' class=$select_style>";
+	}
+	else
+	{
+		echo "<ul>";
+	}
+	
+	foreach($categories as $category) 
+	{		
+		$ID = $category->cat_ID;
+		$subcatcount = getPostCount($ID);
+
+		$category_link = esc_url( get_category_link( $category->term_id ) );
+		$link_title = sprintf( __( "View all posts in %s (%s)" ), $category->name, $subcatcount );
+		$catname = $category->name;
+		
+		echo "
+		<li>
+			<a href='$category_link' title='$link_title'>
+				<div class='category_name'>
+				$catname <span class='category_name_count'>($subcatcount)</span>
+				</div>
+			</a>
+		";
+		
+		$childargs = array(
+			'parent'            => $ID,
+			'hide_empty'        => 1,
+			'hierarchical'      => 0,
+			'pad_counts'        => true
+		);
+		
+		$childcats = get_categories( $childargs );
+		
+		doCategories( $childcats, $select_style, $ID );
+		
+		echo "
+		</li>
+		";	
+	}	
+	
+	echo "</ul>";
+}
+
+class SFCategoryMenuWidget extends WP_Widget {
+
+	function SFCategoryMenuWidget() {
+		// Instantiate the parent object
+		parent::__construct( false, 'SF Category Menu Widget' );
+	}
+
+	function widget( $args, $instance ) 
+	{
+		$exclude_categories = $instance['exclude_cat'];
+		$exclude_categories_arr = explode(",", $exclude_categories);
+		
+		$args = array(
+			'type'                     => 'post',
+			'parent'                   => '0',
+			'orderby'                  => 'name',
+			'order'                    => 'ASC',
+			'hide_empty'               => 1,
+			'hierarchical'             => 0,
+			'exclude'                  => $exclude_categories,
+			'include'                  => '',
+			'number'                   => '',
+			'taxonomy'                 => 'category',
+			'pad_counts'               => true
+		); 
+			
+		$categories = get_categories( $args);
+		$select_style = $instance['select_style'];
+		
+		echo '<div class="dynamic_sidemenu">';
+		
+		doCategories( $categories, $select_style );
+		
+		echo '</div>';
+		
+		?>
+		<script>
+		jQuery(document).ready(function($) {
+			$('#catnavigation').treeview({
+				 collapsed: true,
+				 unique: false,
+				 persist: "location",
+			});
+		});
+		</script>
+		<?php
+
+	}
+
+	function update( $new_instance, $old_instance ) {
+		// Save widget options
+		$instance = $old_instance;
+		$instance['exclude_cat'] = strip_tags($new_instance['exclude_cat']);
+		$instance['select_style'] = strip_tags($new_instance['select_style']);
+
+		return $instance;
+	}
+
+	function form( $instance ) {
+		// Output admin widget options form
+		if( $instance) {
+			 $exclude_cat = esc_attr($instance['exclude_cat']);
+			 $select = esc_attr($instance['select_style']);
+		} else {
+			 $exclude_cat = '';
+			 $select = '';
+		}
+		?>
+	
+		<p>
+		<label for="<?php echo $this->get_field_id('select_style'); ?>"><?php _e('Style:', 'wp_widget_plugin'); ?></label>
+		<select name="<?php echo $this->get_field_name('select_style'); ?>" id="<?php echo $this->get_field_id('select_style'); ?>">
+		<?php
+		$options = array('treeview', 'treeview-red', 'treeview-black', 'treeview-grey', 'treeview-famfamfam');
+		foreach ($options as $option) {
+			echo '<option value="' . $option . '" id="' . $option . '"', $select == $option ? ' selected="selected"' : '', '>', $option, '</option>';
+		}
+		?>
+		</select>
+		</p>
+		
+		<p>
+		<label for="<?php echo $this->get_field_id('exclude_cat'); ?>"><?php _e('Exclude ID:', 'wp_widget_plugin'); ?></label>
+		<input id="<?php echo $this->get_field_id('exclude_cat'); ?>" name="<?php echo $this->get_field_name('exclude_cat'); ?>" type="text" value="<?php echo $exclude_cat; ?>" />
+		</p>
+		<?php		
+	}
+}
+
+function sf_category_menu_widget_register_widgets() {
+	register_widget( 'SFCategoryMenuWidget' );
+}
+
+// Hooks
+
+add_action( 'widgets_init', 'sf_category_menu_widget_register_widgets' );
+
+function load_jquery() {
+    wp_enqueue_script( 'jquery' );
+	
+	wp_enqueue_script('treeview-cookie', plugins_url() . '/sf-category-menu/tree-view/lib/jquery.cookie.js', 'jquery');
+	wp_enqueue_script('treeview', plugins_url() . '/sf-category-menu/tree-view/jquery.treeview.js', 'treeview-cookie');
+	wp_enqueue_style( 'treeview-style', plugins_url() . '/sf-category-menu/tree-view/jquery.treeview.css');
+}
+
+add_action( 'wp_enqueue_scripts', 'load_jquery' );
+
+
+?>
